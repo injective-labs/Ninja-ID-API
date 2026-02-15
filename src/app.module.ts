3dd944fb87c49cad8446702d451544309fc58fd3
YuChanGongzhu <2556553872@qq.com>
2026-02-15 22:23:24 +0800

    Re-enable N1NJ4Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -49,7 +49,7 @@ import { N1NJ4Module } from './n1nj4/n1nj4.module';
     }),
 
     PasskeyModule,
-    // N1NJ4Module, // 临时禁用以测试
+    N1NJ4Module,
   ],
   controllers: [AppController],
   providers: [AppService],
